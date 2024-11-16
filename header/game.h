8c75a83c97fd8acde7b2478bbe29eb64295bcb81
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2024-11-16 12:11:53 -0800

    Added game.h

--- header/game.h
+++ header/game.h
@@ -1,1 +1,22 @@
+#pragma once
 
+#include <string>
+#include <vector>
+using namespace std;
+
+class Game
+{
+    string playerName;
+    string currentWorld;
+    vector<char> guessedLetters;
+    int remainingAttempts;
+    const int maxAttempts;
+    bool gameWon;
+    bool gameLost;
+
+    void startNewGame(const string &word);
+    bool processPlayerGuess(char guess);
+    bool isWordGuessed();
+    string getWordProgress();
+    void endGameMessage(bool won);
+};
