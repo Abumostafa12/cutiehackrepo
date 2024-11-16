36c0bc6a72f45830774094bbb3604b929d330c73
jleyvang <joshln5790@gmail.com>
2024-11-16 12:19:29 -0800

    kdkdkd

--- header/display.h
+++ header/display.h
@@ -1,1 +1,17 @@
+#ifndef display_h
+#define display_h
 
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Display{
+    void DisplayGameState(bool mode);
+    void DisplayHangmanGrid(int wrong); 
+    void DisplayGuessedLetters(string a);   
+};
+
+
+#endif
